365fbbf15ea86f2a29551cae0ca3ef61da4a70d7
krysx7003 <krysx7003@gmail.com>
2025-02-06 01:08:09 +0100

    Theme design + basic view

--- app/src/main/java/com/napnap/testoapp/MainActivity.kt
+++ app/src/main/java/com/napnap/testoapp/MainActivity.kt
@@ -1,14 +1,28 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.napnap.testoapp
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.napnap.testoapp.ui.theme.TestoAppTheme
 
@@ -16,13 +30,26 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TestoAppTheme {
-                // A surface container using the 'background' color from the theme
+            TestoAppTheme(true) {
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize(),
+                        topBar = {
+                            TopAppBar(title = {Text(text = "Test")}, colors = TopAppBarDefaults.topAppBarColors(
+                                containerColor = MaterialTheme.colorScheme.primary,
+                                titleContentColor = Color.White
+                            ) )
+                        }
+                    ) {values ->
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize().padding(values)
+                        ){}
+                    }
+
                 }
             }
         }
@@ -30,17 +57,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CreateTopAppBar(){
+    val context = LocalContext.current.applicationContext
+    TopAppBar(title = {Text(text = "Test")}, colors = TopAppBarDefaults.topAppBarColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        titleContentColor = Color.White
+    ) )
 }
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestoAppTheme {
-        Greeting("Android")
+        CreateTopAppBar()
     }
 }
--- app/src/main/java/com/napnap/testoapp/ui/theme/Color.kt
+++ app/src/main/java/com/napnap/testoapp/ui/theme/Color.kt
@@ -2,10 +2,17 @@ package com.napnap.testoapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val White = Color(0xFFFFFFFF)
+val LightGrey = Color(0xFF9F9F9F)
+
+val BlueGrayBackground= Color(0xFF21252B)
+val BlueGrayMain = Color(0xFF1A1E22)
+
+val BlueBackground = Color(0xFF075098)
+val BlueMain = Color(0xFF06488A)
